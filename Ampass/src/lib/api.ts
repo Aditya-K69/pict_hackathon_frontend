9feda4b99bf96bd1676b0cb6b3d3e9505f44db38
adityakelkar-glory <aditya.kelkar.gloryquick@gmail.com>
2026-01-08 16:01:47 +0530

    Revert "File upload integrated"
    This reverts commit d42531ae94f8a2e4a32118e5f72ec89ea3cd07ab.

--- Ampass/src/lib/api.ts
+++ Ampass/src/lib/api.ts
@@ -1,16 +1,10 @@
-// lib/api.ts
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
-});
-
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem("access_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: "http://localhost:4000", // change if needed
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
