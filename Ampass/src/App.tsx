54764fe4beee21729be5f4520e077727b0146344
mihikasathe20 <mihikasathe.siya@gmail.com>
2026-01-08 12:54:46 +0530

    claim review and submission page

--- Ampass/src/App.tsx
+++ Ampass/src/App.tsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
+import ClaimReview from "./pages/claimreview";
+import ClaimSubmissionSuccess from "./pages/claimsubmissionsuccess";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/claimreview" element={<ClaimReview />} />
+       <Route path="/claimsubmissionsuccess" element={<ClaimSubmissionSuccess />} />
+
+
     </Routes>
   );
 }
