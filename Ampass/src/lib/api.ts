d42531ae94f8a2e4a32118e5f72ec89ea3cd07ab
adityakelkar-glory <aditya.kelkar.gloryquick@gmail.com>
2026-01-08 15:42:33 +0530

    File upload integrated

--- Ampass/src/lib/api.ts
+++ Ampass/src/lib/api.ts
@@ -1,10 +1,16 @@
+// lib/api.ts
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000", // change if needed
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://localhost:4000",
+});
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
